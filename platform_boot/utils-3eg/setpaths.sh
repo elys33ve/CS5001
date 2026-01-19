966f61200f02627380e5b180a15315d5858e95de
elys33ve <elys33ve@gmail.com>
2026-01-19 16:28:09 -0500

    added platform_boot

---
+++ platform_boot/utils-3eg/setpaths.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+unset PLATFORM
+unset KERNELPATH
+unset UBOOTPATH
+unset SDCARD
+
+# axim_ddr_perf
+export PLATFORM=axim_ddr_perf
+export KERNELPATH=~/projects/xilinx/linux-zynqmp-2025.1
+export UBOOTPATH=~/projects/xilinx/u-boot-utils-3eg
+export SDCARD=/media/$USER/boot
+
