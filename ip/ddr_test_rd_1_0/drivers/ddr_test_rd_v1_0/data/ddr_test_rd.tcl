6bb33c50c25acb8a486f2a82de46a54b4ad64245
elys33ve <elys33ve@gmail.com>
2026-01-24 16:21:16 -0500

    removed linux-zynqmp, ip, and platform_boot

--- ip/ddr_test_rd_1_0/drivers/ddr_test_rd_v1_0/data/ddr_test_rd.tcl
+++
@@ -1,5 +0,0 @@
-
-
-proc generate {drv_handle} {
-	xdefine_include_file $drv_handle "xparameters.h" "ddr_test_rd" "NUM_INSTANCES" "DEVICE_ID"  "C_axi_lite_BASEADDR" "C_axi_lite_HIGHADDR"
-}
--- ip/userspace/axim_perf/set_interframe.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo $1 > /sys/bus/platform/devices/80000000.ddr_test/interframe_clks 
-echo $1 > /sys/bus/platform/devices/80020000.ddr_test/interframe_clks 
-echo $1 > /sys/bus/platform/devices/80040000.ddr_test/interframe_clks 
-echo $1 > /sys/bus/platform/devices/80060000.ddr_test/interframe_clks 
-echo $1 > /sys/bus/platform/devices/80080000.ddr_test/interframe_clks 
-echo $1 > /sys/bus/platform/devices/800a0000.ddr_test/interframe_clks 
